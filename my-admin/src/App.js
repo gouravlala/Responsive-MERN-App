4bbc5ef6fcd7c362dbde7af949729f94b54bc013
gouravlala <gouravlala001@gmai.com>
2023-01-10 20:12:29 +0530

    allpage done

--- my-admin/src/App.js
+++ my-admin/src/App.js
@@ -5,15 +5,15 @@ import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashbord";
 import Team from "./scenes/team";
-// import Invoices from "./scenes/invoices";
-// import Contacts from "./scenes/contacts";
+import Invoices from "./scenes/invoices";
+import Contacts from "./scenes/contacts";
+import Form from "./scenes/form";
+import Calendar from "./scenes/calendar";
+import FAQ from "./scenes/faq";
 // import Bar from "./scenes/bar";
-// import Form from "./scenes/form";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
-// import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
-// import Calender from "./scenes/calender";
 
 
 function App() {
@@ -31,15 +31,15 @@ function App() {
             <Routes>
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
-              {/* <Route path="/invoices" element={<Invoices />} /> */}
-              {/* <Route path="/contacts" element={<Contacts />} /> */}
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/faq" element={<FAQ />} />
               {/* <Route path="/bar" element={<Bar />} /> */}
-              {/* <Route path="/form" element={<Form />} /> */}
               {/* <Route path="/line" element={<Line />} /> */}
               {/* <Route path="/pie" element={<Pie />} /> */}
-              {/* <Route path="/faq" element={<FAQ />} /> */}
               {/* <Route path="/geography" element={<Geography />} /> */}
-              {/* <Route path="/calender" element={<Calender />} /> */}
             </Routes>
           </main>
         </div>
